2cca1468a9ec02763b46ddab51771d0293be4a58
Alexandre Resende <aar6947@gmail.com>
2018-03-31 22:37:10 -0300

    chore: Refactor app.js and create components

--- App.js
+++ App.js
@@ -2,11 +2,13 @@
 import React, { Component } from 'react'
 import { AppRegistry, Text, View } from 'react-native'
 
+import Component3 from './app/components/Component3/Component3'
+
 export default class MyApp extends Component {
   render() {
     return(
       <View>
-        <Text>Hello World</Text>
+        <Component3></Component3>
       </View>
     );
   }
